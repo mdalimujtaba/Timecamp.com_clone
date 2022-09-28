b021cd19e2b200fb20cd5411e92cb8c2b5f31994
mdalimujtaba <mdalimujtaba29850@gmil.com>
2022-09-28 23:24:41 +0530

    day2

---
+++ project/src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import { Box } from "@chakra-ui/react";
+import FirstDiv from "../Components/firstDiv";
+import Navbar from "../Components/Navbar";
+
+function Home(){
+    return(
+        <Box>
+            <Navbar/>
+            <FirstDiv/>
+
+        </Box>
+    )
+}
+export default Home 
