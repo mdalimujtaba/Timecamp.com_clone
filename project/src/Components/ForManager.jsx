fe0ee63b4b3ab6ca5f64f3d0c0a87d8b54dce922
mdalimujtaba <mdalimujtaba29850@gmil.com>
2022-09-30 11:18:56 +0530

    edited

--- project/src/Components/ForManager.jsx
+++ project/src/Components/ForManager.jsx
@@ -13,30 +13,30 @@ import {
 } from "@chakra-ui/react";
 export function ForManager() {
   return (
-    <Box
-    //  border={"1px solid black"} 
-    h={["400px"]} w="100%" >
-      
+    <Box 
+    // border={"1px solid black"}
+     h={["fit-content"]} w="100%">
       <SimpleGrid
         columns={[1, 1, 2, 2]}
         // border="1px solid blue"
         w={["80%"]}
         ml={["10%"]}
-        
       >
         <Box
-        //  border={"1px solid blue"} 
-        h={["300px", "300px", "300px", "800px"]} display={['none','none','flex','flex','flex']}>
+          //  border={"1px solid blue"}
+          h={["300px", "300px", "300px", "800px"]}
+          display={["none", "none", "flex", "flex", "flex"]}
+        >
           <Image
             w={["100%"]}
-            height={['90%','90%','60%','50%']}
+            height={["90%", "90%", "60%", "50%"]}
             mt={["10px", "10px", "200px", "200px"]}
             src="https://cdn-m.timecamp.com/img/greenbranding/features/tools-integrations.svg"
           />
         </Box>
         <Box
-        //   border={"1px solid black"}
-          h={["1000px", "700px", "1000px", "800px"]}
+          //   border={"1px solid black"}
+          h={["auto", "700px", "1000px", "800px"]}
         >
           <Text
             color={"rgb(53,211,108)"}
@@ -108,11 +108,11 @@ export function ForManager() {
           <Box
             mt={"100px"}
             w="80%"
+            ml={["10%", "10%", "0%", "0%"]}
             display="flex"
             justifyContent={["center", "center", "flex-start", "flex-start"]}
           >
             <Link _hover={{ textDecoration: "none" }}>
-              {" "}
               <Button
                 _hover={{ bg: "green" }}
                 bg={"rgb(37,207,96)"}
@@ -126,11 +126,13 @@ export function ForManager() {
           </Box>
         </Box>
         <Box
-        //  border={"1px solid blue"}
-          h={["300px", "300px", "300px", "800px"]} display={['flex','flex','none','none','none']}>
+          //  border={"1px solid blue"}
+          h={["300px", "300px", "300px", "800px"]}
+          display={["flex", "flex", "none", "none", "none"]}
+        >
           <Image
             w={["100%"]}
-             height={['90%','90%','60%','50%']}
+            height={["90%", "90%", "60%", "50%"]}
             mt={["10px", "10px", "200px", "200px"]}
             src="https://cdn-m.timecamp.com/img/greenbranding/features/tools-integrations.svg"
           />
